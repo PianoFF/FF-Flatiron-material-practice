c29cc74f81c5b6ea094c823976479153305116b1
PianoFF <Fangfei1star@gmail.com>
2020-04-14 10:18:39 +0100

    complete - proper exponent operator **

--- programming-univbasics-4-square-array-london-web-102819/lib/square_array.rb
+++ programming-univbasics-4-square-array-london-web-102819/lib/square_array.rb
@@ -3,6 +3,7 @@ def square_array(array)
   result = Array.new
   array.length.times do |index|
     result << array[index] * array[index]
+    #exponent: squre ->  array[index] ** 2
   end
   return result 
 end
